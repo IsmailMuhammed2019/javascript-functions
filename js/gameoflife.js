23408e1b6091a7f7a9acbab57225d8aed9688587
Ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2021-10-08 10:38:58 +0100

    finished corners fucntion

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -23,20 +23,23 @@ const printCell = (cell, state) => {
 
 
 const corners = (state = []) => {
-  if (state.length === 0){
-    return {
-      topRight: [0, 0],
-      bottomLeft: [0, 0]
-    };
+  if (state.length === 0) {
+      return {
+          topRight: [0,0],
+          bottomLeft: [0,0]
+      };
   }
 
-  const xs = state.map(([x, _]));
-  const ys = state.map(([_, y]));
+  const xs = state.map(([x,_]) => x);
+  const ys = state.map(([_,y]) => y);
   return {
-    topRight: [Math.max(...xs), Math.max(...ys)],
-    bottomLeft: [Math.max(...xs), Math.max(...ys)]
+      topRight: [Math.max(...xs), Math.max(...ys)],
+      bottomLeft: [Math.min(...xs), Math.min(...ys)]
   };
 };
+
+corners([[1,2], [4,1]]);
+
 
 const printCells = (state) => {};
 
